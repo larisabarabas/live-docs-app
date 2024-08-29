bdf4ca9e732ba1761b9e2b8eb42a50ce59eb2b92
Stefania-Larisa Barabas <stefaniabarabas@gmail.com>
2024-08-29 14:04:29 +0100

    Added liveblocks integration

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,24 +1,11 @@
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/Header";
+import CollaborativeRoom from "@/components/CollaborativeRoom";
 import React from "react";
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Document = () => {
   return (
-    <div>
-      <Header>
-        <div className="flex w-fit items-center justify-center">
-          <p className="document-title"> This a document title </p>
-        </div>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </Header>
-      <Editor />
-    </div>
+    <main className="flex w-full flex-col items-center ">
+      <CollaborativeRoom />
+    </main>
   );
 };
 
